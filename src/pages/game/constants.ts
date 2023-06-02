0419069e03bcf84f06de8c424639a27705f270a8
unknown <andrew_nomie@msn.com>
2023-06-02 09:40:23 +0100

    Add more flags

--- src/pages/game/constants.ts
+++ src/pages/game/constants.ts
@@ -5,13 +5,8 @@ export const MAX_TIME = 60;
 export const FLAGS: ReadonlyArray<Flag> = [
   {
     id: "6-Color Pride",
-    names: ["6-Color Pride", "6 Color", "Pride"],
+    names: ["6-Color Pride", "6 Color", "Pride", "Original"],
     icon: "https://assets.volvo.com/is/image/VolvoInformationTechnologyAB/Color-Pride-Flag?qlt=82&wid=768&ts=1670909834760&dpr=off&fit=constrain&fmt=png-alpha",
-  },
-  {
-    id: "Transgender",
-    names: ["Transgender", "Trans"],
-    icon: "https://assets.volvo.com/is/image/VolvoInformationTechnologyAB/transgender?qlt=82&wid=768&ts=1670910592253&dpr=off&fit=constrain&fmt=png-alpha",
   },
   {
     id: "Progress Pride",
@@ -19,19 +14,9 @@ export const FLAGS: ReadonlyArray<Flag> = [
     icon: "https://assets.volvo.com/is/image/VolvoInformationTechnologyAB/image6?qlt=82&wid=768&ts=1670910647944&dpr=off&fit=constrain&fmt=png-alpha",
   },
   {
-    id: "Nonbinary",
-    names: ["Nonbinary"],
-    icon: "https://assets.volvo.com/is/image/VolvoInformationTechnologyAB/image7?qlt=82&wid=768&ts=1670910694457&dpr=off&fit=constrain&fmt=png-alpha",
-  },
-  {
-    id: "Intersex",
-    names: ["Intersex"],
-    icon: "https://assets.volvo.com/is/image/VolvoInformationTechnologyAB/image8?qlt=82&wid=768&ts=1670910744576&dpr=off&fit=constrain&fmt=png-alpha",
-  },
-  {
-    id: "Asexual",
-    names: ["Asexual"],
-    icon: "https://assets.volvo.com/is/image/VolvoInformationTechnologyAB/image9?qlt=82&wid=768&ts=1670910792945&dpr=off&fit=constrain&fmt=png-alpha",
+    id: "Lesbian",
+    names: ["Lesbian"],
+    icon: "https://assets.volvo.com/is/image/VolvoInformationTechnologyAB/image12?qlt=82&wid=768&ts=1670910940053&dpr=off&fit=constrain&fmt=png-alpha",
   },
   {
     id: "Bisexual",
@@ -39,13 +24,38 @@ export const FLAGS: ReadonlyArray<Flag> = [
     icon: "https://assets.volvo.com/is/image/VolvoInformationTechnologyAB/image10?qlt=82&wid=768&ts=1670910840541&dpr=off&fit=constrain&fmt=png-alpha",
   },
   {
+    id: "Gay Men",
+    names: ["Gay Men", "MLM"],
+    icon: "https://assets.volvo.com/is/image/VolvoInformationTechnologyAB/image14?qlt=82&wid=768&ts=1670911241536&dpr=off&fit=constrain&fmt=png-alpha",
+  },
+  {
+    id: "Transgender",
+    names: ["Transgender", "Trans"],
+    icon: "https://assets.volvo.com/is/image/VolvoInformationTechnologyAB/transgender?qlt=82&wid=768&ts=1670910592253&dpr=off&fit=constrain&fmt=png-alpha",
+  },
+  {
+    id: "Asexual",
+    names: ["Asexual"],
+    icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Asexual_Pride_Flag.svg/1280px-Asexual_Pride_Flag.svg.png",
+  },
+  {
+    id: "Nonbinary",
+    names: ["Nonbinary", "Non-Binary", "Non Binary"],
+    icon: "https://assets.volvo.com/is/image/VolvoInformationTechnologyAB/image7?qlt=82&wid=768&ts=1670910694457&dpr=off&fit=constrain&fmt=png-alpha",
+  },
+  {
+    id: "Genderqueer",
+    names: ["Gender Queer", "Genderqueer"],
+    icon: "https://assets.volvo.com/is/image/VolvoInformationTechnologyAB/768px-Genderqueer-Pride-Flag?qlt=82&wid=768&ts=1670916979657&dpr=off&fit=constrain&fmt=png-alpha",
+  },
+  {
+    id: "Intersex",
+    names: ["Intersex"],
+    icon: "https://assets.volvo.com/is/image/VolvoInformationTechnologyAB/image8?qlt=82&wid=768&ts=1670910744576&dpr=off&fit=constrain&fmt=png-alpha",
+  },
+  {
     id: "Pansexual",
     names: ["Pansexual", "Pan", "Pans"],
     icon: "https://assets.volvo.com/is/image/VolvoInformationTechnologyAB/image11?qlt=82&wid=768&ts=1670910888857&dpr=off&fit=constrain&fmt=png-alpha",
-  },
-  {
-    id: "Lesbian",
-    names: ["Lesbian"],
-    icon: "https://assets.volvo.com/is/image/VolvoInformationTechnologyAB/image12?qlt=82&wid=768&ts=1670910940053&dpr=off&fit=constrain&fmt=png-alpha",
   },
 ];
