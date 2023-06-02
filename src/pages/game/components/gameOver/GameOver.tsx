e3009fb0006452a7a4617317754a8ca92c8629e8
unknown <andrew_nomie@msn.com>
2023-06-02 14:42:25 +0100

    Fix results string

--- src/pages/game/components/gameOver/GameOver.tsx
+++ src/pages/game/components/gameOver/GameOver.tsx
@@ -28,7 +28,11 @@ function GameOver({ gameResults, onPlayClick, onGuideClick }: GameOverProps) {
       <div className="flex justify-center content-center">
         <h1 className="text-4xl font-bold pb-4">{titleText}</h1>
       </div>
-      <div className="flex justify-center content-center pb-6">{`You guessed ${gameResults.score} flags corrects! Want to play again?`}</div>
+      <div className="flex justify-center content-center pb-6">{`You guessed ${
+        gameResults.score
+      } ${
+        gameResults.score === 1 ? "flag" : "flags"
+      } correctly! Want to play again?`}</div>
       <div className="flex justify-center content-center">
         <button
           className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
